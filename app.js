91d23851b43e57e3061ca34857f0875e28f6827f
wjbuena <wilsonjeffrey.buena@synpulse.com>
2022-07-19 12:39:16 +0800

    Project setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            books: [
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss'},
+                { title: 'The Way of Kings', author: 'Brandon Sanderson'},
+                { title: 'The Final Empire', author: 'Patrick Rothfuss'},
+            ]
+        }
+    },
+
+    methods: {
+       toggleShowBooks() {
+        this.showBooks = !this.showBooks;
+       },
+
+       
+
+    }
+});
+
+app.mount('#app');
